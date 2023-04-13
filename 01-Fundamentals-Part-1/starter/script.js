552f5a1fce09e14e959867ac95b36a5844a7d68e
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-04-13 09:01:35 +0200

    section2 complete

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,53 @@
+// let country = 'France';
+// let continent = 'Europe';
+// let population = 65;
+
+// console.log(country, continent, population);
+
+
+// //Data Types
+// let isIsland = false;
+// let language;
+// console.log(typeof isIsland);
+// console.log(typeof country);
+// console.log(typeof population);
+// console.log(typeof language);
+
+// // Operators
+
+// populationHalf = population / 2;
+// console.log(populationHalf);
+// population++;
+// console.log(population);
+// console.log(population < 50);
+
+
+// Coding Challenge 1 & 2
+
+// const johnMass = 92;
+// const markMass = 78;
+// const johnHeight = 1.95;
+// const markHeight = 1.69;
+
+// const johnBMI = johnMass / (johnHeight ** 2);
+// const markBMI = markMass / (markHeight ** 2);
+
+// console.log(typeof johnBMI)
+// console.log(johnBMI, markBMI);
+
+// const markHigherBMI = (markBMI > johnBMI);
+// console.log(markHigherBMI);
+
+// if(markHigherBMI) {
+//   console.log(`Mark's BMI (${Math.round(markBMI)}) is higher than John's BMI (${Math.round(johnBMI)}) !`)
+// } else {
+//   console.log(`John's BMI (${Math.round(johnBMI)}) is higher than Mark's BMI (${Math.round(markBMI)})!`)
+// }
+
+// console.log(Math.round(27.8))
+
+// Coding Challenge 4
+
+let bill = 400;
+const tip = (bill >= 50 && bill < 300) ? bill * 0.15 : bill * 0.20;
+console.log(tip);
