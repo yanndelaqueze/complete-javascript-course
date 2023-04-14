4f54bbe9ee15e15638dbc2a6569ccfd38998a8d5
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-04-14 10:23:33 +0200

    s03

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,17 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const array1 = [17, 21, 23];
+const array2 = [12, 5, -5, 0, 4];
+
+const printForecast = (array) => {
+  let textArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const text = `${array[i]}°C in ${i + 1} days... `;
+    textArray.push(text);
+  }
+  return textArray.join('');
+};
+
+console.log(printForecast(array1));
+console.log(printForecast(array2));
