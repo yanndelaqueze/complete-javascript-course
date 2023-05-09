7b332811f30d19a65d152bb223060dcda402afe9
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-05-09 11:18:35 +0200

    Guess My Number - WIP

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,56 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = '?';
+
+let score = 20;
+
+const resetButton = document.querySelector('.again');
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫 No Number !!';
+
+    // guess is correct
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number !!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    // guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '⬇️ Too Low !!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '👎 You Lose !!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '⬆️ Too High !!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '👎 You Lose !!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+resetButton.addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
