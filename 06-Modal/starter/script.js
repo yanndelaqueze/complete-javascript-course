2391bbb6a74f48686c9e7998a526db8ec43d9951
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-05-09 13:21:41 +0200

    pig game - initial commit

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -18,3 +18,9 @@ const closeModal = () => {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
