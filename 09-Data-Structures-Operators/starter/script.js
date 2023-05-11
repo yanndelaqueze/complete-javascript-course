892a286bae0706343adef43a97fc3efab38dac62
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-05-11 21:05:51 +0200

    challenge 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -140,4 +140,55 @@ for (const player of game.scored) {
   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
 }
 
-console.log(scorers);
+// console.log(scorers);
+
+// Challenge 3
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [(72, '🔁 Substitution')],
+  [(76, '⚽ GOAL')],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+//1 Create an array 'events'ofthedifferentgameeventsthathappened(no duplicates)
+
+const events = [];
+for (const event of gameEvents) {
+  if (!events.includes(event[1])) {
+    events.push(event[1]);
+  }
+}
+console.log(events);
+
+const events2 = [...new Set(gameEvents.values())];
+console.log(events2);
+
+// 2 Afterthegamehasfinished,iswasfoundthattheyellowcardfromminute64 was unfair. So remove this event from the game events log.
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3 Computeandlogthefollowingstringtotheconsole:"Aneventhappened,on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+console.log(
+  `An event happened on average every ${90 / gameEvents.size} minutes`
+);
+
+//4 Loop over 'gameEvents' and log each element to the console ,marking whether it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17: ⚽   GOAL
+
+for (const [key, event] of gameEvents) {
+  if (key < 45) {
+    console.log(`[FIRST HALF] ${key} : ${event}`);
+  } else {
+    console.log(`[SECOND HALF] ${key} : ${event}`);
+  }
+}
