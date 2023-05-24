246b8dcf7e7cb15958982e78b08193ec832a2250
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-05-24 14:26:57 +0200

    OOP - Challenge 4

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -98,48 +98,108 @@ DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
 GOOD LUCK 😀
 */
 
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(
-    `${this.make} is going at ${this.speed}km/h with a charge of ${this.charge}`
-  );
-};
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(
+//     `${this.make} is going at ${this.speed}km/h with a charge of ${this.charge}`
+//   );
+// };
 
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(
-    `${this.make} is going at ${this.speed}km/h with a charge of ${this.charge}`
-  );
-};
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(
+//     `${this.make} is going at ${this.speed}km/h with a charge of ${this.charge}`
+//   );
+// };
 
-const EV = function (make, speed, charge) {
-  Car.call(this, make, speed);
-  this.charge = charge;
-};
+// const EV = function (make, speed, charge) {
+//   Car.call(this, make, speed);
+//   this.charge = charge;
+// };
 
-const tesla = new EV('Tesla', 120, 23);
+// const tesla = new EV('Tesla', 120, 23);
 
-EV.prototype = Object.create(Car.prototype);
-EV.prototype.chargeBattery = function (chargeTo) {
-  this.charge = chargeTo;
-};
-EV.prototype.accelerate = function () {
-  this.speed += 20;
-  this.charge -= 1;
-  console.log(
-    `${this.make} is going at ${this.speed}km/h with a charge of ${this.charge}`
-  );
-};
+// EV.prototype = Object.create(Car.prototype);
+// EV.prototype.chargeBattery = function (chargeTo) {
+//   this.charge = chargeTo;
+// };
+// EV.prototype.accelerate = function () {
+//   this.speed += 20;
+//   this.charge -= 1;
+//   console.log(
+//     `${this.make} is going at ${this.speed}km/h with a charge of ${this.charge}`
+//   );
+// };
 
-tesla.accelerate();
-tesla.accelerate();
-tesla.accelerate();
+// tesla.accelerate();
+// tesla.accelerate();
+// tesla.accelerate();
 
-tesla.brake();
-tesla.brake();
-tesla.brake();
+// tesla.brake();
+// tesla.brake();
+// tesla.brake();
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(this.speed);
+    return this;
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is now going at ${this.speed}km/h `);
+    return this;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    console.log(`Battery is back to ${chargeTo}%`);
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    this.#charge -= 1;
+    console.log(
+      `${this.make} is going at ${this.speed}km/h with a charge of ${
+        this.#charge
+      }`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+
+rivian.accelerate().chargeBattery(100).accelerate().brake();
